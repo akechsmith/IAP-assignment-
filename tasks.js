6e6290c7f6ef1e5dfe5ef1c5956508041a0944a1
akech atem <97437834+akechsmith@users.noreply.github.com>
2023-11-24 14:47:42 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,28 +1,6 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     let submit = document.querySelector('input[type="submit"]');
-//     let newTask = document.querySelector('#task');
-//     let tasksList = document.querySelector('#tasks');
-  
-//     submit.disabled = true;
-  
-//     newTask.addEventListener('input', function () {
-//       submit.disabled = newTask.value.length === 0;
-//     });
-  
-//     document.querySelector('form').addEventListener('submit', function (event) {
-//       event.preventDefault();
-  
-//       let newTaskText = newTask.value.trim();
-  
-//       if (newTaskText.length > 0) {
-//         let newTaskItem = document.createElement('li');
-//         newTaskItem.innerText = newTaskText;
-//         tasksList.appendChild(newTaskItem);
-//         newTask.value = '';
-//         submit.disabled = true;
-//       }
-//     });
-//   });
+// NAME: AKECH DAU ATEM 
+//
+// REGno: SCT211-0535/2022
   
 document.addEventListener('DOMContentLoaded', function () {
   let submitButton = document.getElementById('submit');
